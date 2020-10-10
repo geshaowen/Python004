3142b9ccdb10272f43b2676b935d6ad45fddde87
geshaowen <a.geshaowen@gmail.com>
2020-10-10 16:45:27 +0800

    完成作业二

--- Week01/maoyan/maoyan/pipelines.py
+++ Week01/maoyan/maoyan/pipelines.py
@@ -9,14 +9,18 @@ import pandas as pd
 
 class MaoyanPipeline:
     def process_item(self, item, spider):
-        title = item['title']
-        tags = item['tags']
-        play_time = item['play_time']
-
-        movie_detail = [title, tags, play_time]
-
-        movie_file = pd.Dataframe(data = movie_detail)
-
-        movie_file.to_csv('./maoyan.csv', mode='a', encoding = 'utf-8', index=False, header=False)
-
         return item
+
+
+    # def process_item(self, item, spider):
+    #     title = item['title']
+    #     tags = item['tags']
+    #     play_time = item['play_time']
+
+    #     movie_detail = [title, tags, play_time]
+
+    #     movie_file = pd.Dataframe(data = movie_detail)
+
+    #     movie_file.to_csv('./maoyan.csv', mode='a', encoding = 'utf-8', index=False, header=False)
+
+    #     return item
--- Week01/maoyan/maoyan/spiders/maoyanmovies.py
+++ Week01/maoyan/maoyan/spiders/maoyanmovies.py
@@ -17,32 +17,19 @@ class MaoyanmoviesSpider(scrapy.Spider):
 
 
     def parse(self, response):
-        movies = Selector(response=response).xpath('//div[@class="movies-list"]')
-        for i in range(10):
-            # title = movies[i].xpath('./span[1]/text()')
-            # tags = movies[i].xpath('./span[3]/text()')
-            # play_time = movies[i].xpath('./span[4]/text()')
-            
-            link = 'https://maoyan.com' + movies[i].xpath('./a/@href').extract_first()
-            yield scrapy.Request(url=link, callback=self.parse2)
-    
-
-    def parse2(self, response):
-        movie_info = Selector(response=response).xpath('//div[@class="movie-brief-container"]')
+        movies = Selector(response=response).xpath('//div[@class="movie-hover-info"]')
 
         item = MaoyanItem()
 
-        # 电影名称
-        title = movie_info.xpath('./h1/text()')
+        for i in range(10):
+             title = movies[i].xpath('./div[1]/@title').extract_first()
+             tags = movies[i].xpath('./div[2]/text()').extract()[1].strip()
+             play_time = movies[i].xpath('./div[4]/text()').extract()[1].strip()
 
-        # 电影类型
-        tags = movie_info.xpath('./ul/li[1]/a/text()')
+             item['title'] = title
+             item['tags'] = tags
+             item['play_time'] = play_time
 
-        # 上映日期
-        play_time = movie_info.xpath('./ul/li[3]/text()')
+             yield item
+    
 
-        item['title'] = title
-        item['tags'] = tags
-        item['play_time'] = play_time
-
-        yield item
